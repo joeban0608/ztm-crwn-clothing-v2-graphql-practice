03ebe0cc25097bbdc0e9d0d29b0d9d7773e5c755
joebanhong <joeanddaneil5@gmail.com>
2023-02-07 23:00:51 +0800

    feat: 引用 apollo 至專案 - add package @apollo/client, graphql - initial apollo in index.js

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,24 +1,46 @@
-import { createContext, useState, useEffect } from 'react';
+import { useQuery, gql } from "@apollo/client";
+import { createContext, useState, useEffect } from "react";
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
+  console.log("data", data);
+
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
+    if (data) {
+      const { collections } = data;
+      const categoryMap = collections.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
       setCategoriesMap(categoryMap);
-    };
+    }
+  }, [data]);
 
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
